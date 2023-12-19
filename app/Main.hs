58935bc858658e556d5c61f57803bb2874205b5c
nelsonm2991 <nelsonm2991@gmail.com>
2023-12-19 17:35:04 -0500

    Added depth information for each file from starting directory

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,9 @@ import Traversal
 main :: IO ()
 main = do
   --putStrLn "before hello"
-  list <- traverseFS "/Users/matthew/PFP/fs-project"
+  list <- traverseFS 0 "/Users/matthew/PFP/fs-project"
   -- Issue: we have an error when calling traverseFS that we are failing to deal with
   --putStrLn "hello"
+  mapM_ (putStrLn . show) list
+
   return ()
--- src/Traversal.hs
+++ src/Traversal.hs
@@ -6,22 +6,38 @@ import System.Directory
 import System.Posix.Files
 import System.Posix.Types
 import Control.Monad(forM, guard)
+import Data.List(intercalate)
 
 data FileInfo = FileInfo { fPath :: FilePath
-                         , stats :: FileStats }
+                         , stats :: FileStats
+                         , depth :: Int }
 
 data FileStats = FileStats { devID :: DeviceID
                            , fSize :: FileOffset
                            , isDir :: Bool }
+
+-- TODO: add Show instances for FileInfo and FileStats
+{-
+instance Show Product where
+  show = printFields [show . pProdID, pName, show . pPrice]
+-}
+printFields :: [a -> String] -> a -> String
+printFields functs object = intercalate " " $ map ($ object) functs
+
+instance Show FileStats where
+  show = printFields [show . devID, show . fSize, show . isDir]
+
+instance Show FileInfo where
+  show = printFields [show . fPath, show . stats, show . depth]
 
 {-
 Library for traversing the file system to collect [[FilePath : FileStats]]
 adjacency lists
 -}
 
-traverseFS :: FilePath -> IO [FileInfo]
-traverseFS path = do
-  putStrLn ("considering: " ++ path)
+traverseFS :: Int -> FilePath -> IO [FileInfo]
+traverseFS depth path = do
+  --putStrLn ("considering: " ++ path)
   exists <- doesDirectoryExist path
   case exists of
     True -> do
@@ -30,25 +46,27 @@ traverseFS path = do
 
       -- Convert paths to file information, and get seperate list for sub-directories
       let filePaths = map (\fileName -> path ++ "/" ++ fileName) dirContents
-      fileInfos <- getFileInfos filePaths
+      fileInfos <- getFileInfos filePaths depth
       let subDirsInfos = [subDir | subDir <- fileInfos, (isDir (stats subDir))]
       let subDirPaths = [(fPath subDir) | subDir <- subDirsInfos]
 
       -- Recursively explore the sub-directories and grab their [FileInfo]s
-      overallResult <- concatMapM (traverseFS) subDirPaths
+      overallResult <- concatMapM (traverseFS (depth + 1)) subDirPaths
 
       return (fileInfos ++ overallResult)
     False -> do
       -- FileInfo for this file already collected via parent directory, so ignore it
       return []
 
-getFileInfos :: [FilePath] -> IO [FileInfo]
-getFileInfos filePaths = forM filePaths $ \filePath -> do
+getFileInfos :: [FilePath] -> Int -> IO [FileInfo]
+getFileInfos filePaths fileDepth = forM filePaths $ \filePath -> do
   fileStatus <- getFileStatus filePath
   let toFileInfo :: FilePath -> FileStatus -> FileInfo
-      toFileInfo filePath fileStatus = FileInfo { fPath = filePath, stats = FileStats { devID = (deviceID fileStatus)
-                                                                                     , fSize = (fileSize fileStatus)
-                                                                                     , isDir = (isDirectory fileStatus)}}
+      toFileInfo filePath fileStatus = FileInfo { fPath = filePath
+                                                , stats = FileStats { devID = (deviceID fileStatus)
+                                                                    , fSize = (fileSize fileStatus)
+                                                                    , isDir = (isDirectory fileStatus)}
+                                                , depth = fileDepth}
   return (toFileInfo filePath fileStatus)
 
 -- Taken from: https://hackage.haskell.org/package/extra-1.7.14/docs/src/Control.Monad.Extra.html#concatMapM
