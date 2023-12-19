f5a6a12ac1ba936fd3e1ce8f3b72dee7d14ddeef
nelsonm2991 <nelsonm2991@gmail.com>
2023-12-19 15:50:24 -0500

    Basic directory open

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,9 @@
 module Main (main) where
 
 import Lib
+import Traversal
 
 main :: IO ()
-main = someFunc
+main = do
+  list <- traverseFS "/home" []
+  return ()
--- src/Traversal.hs
+++ src/Traversal.hs
@@ -1,14 +1,55 @@
 module Traversal
-    ( someOtherFunc
+    ( traverseFS
     ) where
 
+import System.Directory
 import System.Posix.Files
 import System.Posix.Types
 
-someOtherFunc :: IO ()
-someOtherFunc = putStrLn "someOtherFunc"
+data FileInfo = FileAllStats { path :: FilePath
+                                 , stats :: FileStats }
+
+data FileStats = FileStats { deviceID :: DeviceID
+                           , fileSize :: FileOffset
+                           , isDir :: Bool }
+
+-- Todo: add instance Show for FileStats for easy output
 
 {-
 Library for traversing the file system to collect [[FilePath : FileStats]]
 adjacency lists
+-}
+
+-- Traverse the file system to the given depth. Collection [FilePath : FilesStats]
+-- mapping and append them to the final returned FilePath
+
+traverseFS :: FilePath -> [FileInfo] -> IO [FileInfo]
+traverseFS path infoList = do
+  exists <- doesDirectoryExist path
+  case exists of
+    True -> do
+      -- Get the contents of the directory
+      --dirContents <- listDirectory path
+      -- For now, try to output the contents of each path encountered
+      --putStrLn (dirContents)
+      putStrLn "hello"
+      return []
+    False -> return []
+
+
+
+{-
+readTableFile :: String -> (Handle -> IO a) -> IO [a]
+readTableFile filename function = withFile filename ReadMode $ \mainHandle -> do
+    let readContents :: Handle -> (Handle -> IO a) -> IO [a]
+        readContents handle func = do
+            eof <- hIsEOF handle
+            case eof of
+                True -> return []
+                False -> do
+                    currLine <- func handle
+                    remaining <- readContents handle func
+                    return (currLine : remaining)
+    vals <- readContents mainHandle function
+    return vals
 -}
